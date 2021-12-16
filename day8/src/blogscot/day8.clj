36035cdf30f21e6715ab784de5ff2fc19cd52aa6
Iain Diamond <iain.diamond@gmail.com>
2021-12-16 13:59:46 +0000

    Refactor (prefer destructuring)

--- day8/src/blogscot/day8.clj
+++ day8/src/blogscot/day8.clj
@@ -24,15 +24,15 @@
         groups (group-by count line)
         {[one] 2 [seven] 3 [four] 4 five-segments 5 six-segments 6 [eight] 7} groups
         c-and-f (set/intersection seven one)
-        three (first (filter #(set/superset? % c-and-f) five-segments))
-        six (first (remove #(set/superset? % c-and-f) six-segments))
+        [three] (filter #(set/superset? % c-and-f) five-segments)
+        [six] (remove #(set/superset? % c-and-f) six-segments)
         third-segment (first (set/difference eight six))
         two-or-five (remove (partial = three) five-segments)
         zero-or-nine (remove (partial = six) six-segments)
-        five (first (remove #(contains? % third-segment) two-or-five))
-        two (first (filter #(contains? % third-segment) two-or-five))
+        [five] (remove #(contains? % third-segment) two-or-five)
+        [two] (filter #(contains? % third-segment) two-or-five)
         nine (conj five third-segment)
-        zero (first (remove (partial = nine) zero-or-nine))
+        [zero] (remove (partial = nine) zero-or-nine)
         mappings (assoc {} zero 0 one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9)]
     (map mappings output-values)))
 
